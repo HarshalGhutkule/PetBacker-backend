ff0000808703f72ba58dcb0b0476a912ee5eb375
Harshal Ghutkule <95927895+HarshalGhutkule@users.noreply.github.com>
2023-06-16 20:09:10 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+mongoose.set('strictQuery', false);
+
 const connect = ()=>{
     return mongoose.connect(
         "mongodb+srv://harshal2993:Welcome_1021@cluster0.ylf9n.mongodb.net/petbacker"
